63518e3e47089eed9cdaec702f07b69d00e1e0a3
oakuzmina <oakuzmina@asuproject.ru>
2023-09-15 20:42:56 +0300

    25,27,29

--- Program.cs
+++ Program.cs
@@ -5,60 +5,64 @@ class Program
     static void Main()
     {
         Console.WriteLine("Выберите задачу:");
-        Console.WriteLine("1. выводит третью цифру заданного числа или сообщает, что третьей цифры нет.");
-        Console.WriteLine("2. принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным");
+        Console.WriteLine("1. принимает на вход два числа (A и B) и возводит число A в натуральную степень B.");
+        Console.WriteLine("2. принимает на вход число и выдаёт сумму цифр в числе");
+        Console.WriteLine("3. задаёт массив из 8 элементов и выводит их на экран");
 
         int choice = int.Parse(Console.ReadLine());
 
         switch (choice)
         {
             case 1:
-                ThirdDigit();
+                APowB();
                 break;
             case 2:
-                WeekEnd();
+                DigitSum();
+                break;
+            case 3:
+                ArrayEight();
                 break;
             default:
                 Console.WriteLine("Неверный выбор.");
                 break;
         }
     }
-    static void ThirdDigit()
-    {        
-        Console.Write("Введите целое число: ");
-        if (int.TryParse(Console.ReadLine(), out int num))
-        {
-        string numStr = num.ToString();
+    static void APowB()
+    {
+        Console.WriteLine("Введите число A:");
+        double A = Convert.ToDouble(Console.ReadLine());
 
-        if (numStr.Length >= 3)
-        {
-            char thirdChar = numStr[2];
-            Console.WriteLine(thirdChar);
-        } else {
-            Console.WriteLine("Третьей цифры нет.");
-        }} else {
-            Console.WriteLine("Не число.");
-        }
+        Console.WriteLine("Введите степень B:");
+        int B = Convert.ToInt32(Console.ReadLine());
 
+        double result = Math.Pow(A, B);
+        Console.WriteLine($"{A}^{B} = {result}");
     }
+    static void DigitSum()
+    {
+        Console.WriteLine("Введите число:");
+        int number = Convert.ToInt32(Console.ReadLine());
 
-    static void WeekEnd() {
- 
-        Console.Write("Введите день недели: ");
-        if (int.TryParse(Console.ReadLine(), out int weekDay))
+        int sum = 0;
+        while (number > 0)
         {
-            if (weekDay<1||weekDay>7) {
-             Console.WriteLine("Не день недели.");
-           } else {
-            if (weekDay>5) {
-             Console.WriteLine("да");
-            } else {
-             Console.WriteLine("нет");
-            }
-
-           }
-        } else {
-            Console.WriteLine("не число");
+            int digit = number % 10;
+            sum += digit;
+            number /= 10;
         }
+        Console.WriteLine($"Сумма цифр в числе: {sum}");
+    }
+    static void ArrayEight()
+    {
+       int[] arr = new int[8];
+       Random random = new Random();
+
+        for (int i = 0; i < 8; i++)
+        {
+            arr[i] = random.Next(1, 100); // Генерация случайного числа от 1 до 99
+        }
+
+        Console.WriteLine("Массив:");
+        Console.WriteLine("[" + string.Join(", ", arr) + "]");
     }
 }
